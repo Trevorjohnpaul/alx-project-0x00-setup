1fceecabd586cf9fadd2331de733e173bed29ae5
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-11-26 23:14:28 +0300

    feat:Advanced task

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`bg-blue-500 text-white px-4 py-2 font-medium transition duration-200 hover:bg-blue-600 ${
+        styles || ""
+      }`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
@@ -9,6 +10,18 @@ const Landing: React.FC = () => {
       <Card />
       <Card />
       <Card />
+
+      <div className="flex flex-col gap-4 mt-4">
+        /* Small buttons */
+        <Button title="Small Rounded" styles="px-2 py-1 rounded-sm" />
+        <Button title="Small Full" styles="px-2 py-1 rounded-full" />
+        /* Medium buttons */
+        <Button title="Medium Rounded" styles="px-4 py-2 rounded-md" />
+        <Button title="Medium Full" styles="px-4 py-2 rounded-full" />
+        /* Large buttons */
+        <Button title="Large Rounded" styles="px-6 py-3 rounded-md" />
+        <Button title="Large Full" styles="px-6 py-3 rounded-full" />
+      </div>
     </div>
   );
 };
