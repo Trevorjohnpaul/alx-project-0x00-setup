c9b5d62bb2239af130bcc86a7c055c4af8d9b6a4
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-11-26 23:22:08 +0300

    feat :changes

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -4,15 +4,15 @@ import Button from "@/components/Button";
 const Landing: React.FC = () => {
   return (
     <div className="p-4 flex flex-col gap-4">
-      /* Small Buttons */
+      {/* Small Buttons */}
       <Button title="Small Rounded" styles="px-2 py-1 rounded-sm" />
       <Button title="Small Full" styles="px-2 py-1 rounded-full" />
       <Button title="Small Large" styles="px-2 py-1 rounded-lg" /> {/* added */}
-      /* Medium Buttons */
+      {/* Medium Buttons */}
       <Button title="Medium Rounded" styles="px-4 py-2 rounded-md" />
       <Button title="Medium Full" styles="px-4 py-2 rounded-full" />
       <Button title="Medium Large" styles="px-4 py-2 rounded-lg" /> /* Large
-      Buttons */
+      {/*Buttons */}
       <Button title="Large Rounded" styles="px-6 py-3 rounded-md" />
       <Button title="Large Full" styles="px-6 py-3 rounded-full" />
       <Button title="Large Large" styles="px-6 py-3 rounded-lg" /> {/* added */}
