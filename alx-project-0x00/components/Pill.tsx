95d41ad928fd5845797bd407978ffac5b7a7e0b3
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-11-26 23:11:33 +0300

    feat:functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-
-interface PillProps {
-  title: string; // this allows the dynamic string for each Pill
-}
+import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <span className="bg-gray-200 text-gray-800 px-3 py-1 rounded-full text-sm">
-      {title}
-    </span>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
+    </div>
   );
 };
 
